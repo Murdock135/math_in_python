c9757e97524aea53bb28f24ee6d0a728a61bff8b
particlegriffin <particlegriffin@gmail.com>
2023-12-07 19:45:32 -0600

    regular update

--- gram_schmidt.py
+++ gram_schmidt.py
@@ -4,20 +4,23 @@ def gs(v):
     w = v.copy() # copy basis
 
     # start gram-schmidt process to get orthogonal basis
-    for i in range(1,len(v)):
+    for i in range(1,len(w)):
         for j in range(i):
-            w[i] = w[i] - np.dot( np.dot( v[i],w[j] ) / np.dot( w[j],w[j] ), w[j] )
+            w[i] = w[i] - np.dot( np.dot( w[i],w[j] ) / np.dot( w[j],w[j] ), w[j] ) # if w[i] is w[2], w[j] is w[1]
 
-    # Get orthonormal basis
+    # Convert to unit vectors
     w = [vec/np.linalg.norm(vec) for vec in w]
 
     return w
 
 
 
-v1, v2, v3 = [1,-1,0], [1,2,1], [0,1,1]
-v = [v1,v2,v3]
-print(gs(v))
+# v1, v2, v3, v4 = [1,-1,0], [1,2,1], [0,1,1]
+# v = [v1,v2,v3]
+print(gs([[-1,1,0,2],
+          [1,-1,-1,0],
+          [2,0,1,1],
+          [1,0,0,1]]))
 
 
 
